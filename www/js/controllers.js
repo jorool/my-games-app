e3c82509f4dae6208c34fa2bd530faaeb501ec8d
Jonathan <jorool1988@gmail.com>
2016-02-01 23:22:21 -0200

    Getting the platform selection done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,8 @@
 angular.module('app.controllers', [])
 
-    .controller('gamesCtrl', function ($scope) {
+    .controller('gamesCtrl', function ($scope, Platform) {
+
+        $scope.platforms = Platform.getMyPlatforms();
 
         $scope.createGame = function () {
             $scope.creatingGame = true;
@@ -8,13 +10,14 @@ angular.module('app.controllers', [])
         };
 
         $scope.cancel = function () {
-            console.log('cancel');
             $scope.creatingGame = false;
-            $scope.game = undefined;
+            delete $scope.game;
         };
 
-        $scope.save = function () {
-            console.log('save');
+        $scope.save = function (game) {
+            console.log(game);
+            $scope.creatingGame = false;
+            delete $scope.game
         };
 
     })
@@ -23,12 +26,47 @@ angular.module('app.controllers', [])
 
     })
 
-    .controller('platformsCtrl', function ($scope, Platform) {
+    .controller('platformsCtrl', function ($scope, Platform, $ionicPopup) {
 
-        Platform.getPlatforms(function (response) {
-            $scope.platforms = response.data;
-        });
+        function loadPlatforms() {
+            Platform.getPlatforms(function (response) {
+                var platforms = response.data;
+                var myPlatforms = Platform.getMyPlatforms();
 
+                angular.forEach(platforms, function (platform) {
+                    angular.forEach(myPlatforms, function (myPlatform) {
+                        if (myPlatform.id == platform.id) {
+                            platform.selected = true;
+                        }
+                    });
+                });
+
+                $scope.platforms = platforms;
+            });
+        }
+
+        $scope.togglePlatform = function (platform) {
+
+            if (platform.selected) {
+                Platform.addToMyPlatforms(platform, function () {
+                    $ionicPopup.alert({
+                        title: 'Sucesso!',
+                        template: 'Plataforma adicionada.'
+                    });
+                });
+            } else {
+                Platform.removeFromMyPlatforms(platform, function () {
+                    loadPlatforms();
+                    $ionicPopup.alert({
+                        title: 'Sucesso!',
+                        template: 'Plataforma removida.'
+                    });
+                });
+            }
+
+        };
+
+        loadPlatforms();
     })
 
     .controller('configCtrl', function ($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -13,15 +13,47 @@ angular.module('app.services', [])
                 });
         }
 
+        function getMyPlatforms() {
+            return JSON.parse(localStorage.getItem('my-games-platforms'));
+        }
+
+        function addToMyPlatforms(platform, callback) {
+            var platforms = JSON.parse(localStorage.getItem('my-games-platforms'));
+            if (!platforms) {
+                platforms = [];
+            }
+            platforms.push(platform);
+            localStorage.setItem('my-games-platforms', JSON.stringify(platforms));
+            callback();
+        }
+
+        function removeFromMyPlatforms(platform, callback) {
+            var platforms = JSON.parse(localStorage.getItem('my-games-platforms'));
+            angular.forEach(angular.copy(platforms), function (p, index) {
+                if (p.id == platform.id) {
+                    platforms.splice(index, 1);
+                }
+            });
+            localStorage.setItem('my-games-platforms', JSON.stringify(platforms));
+            callback();
+        }
+
         return {
             getPlatforms: function (callback) {
                 return getPlatforms(callback);
             },
 
-            addPlatform: function () {
+            getMyPlatforms: function () {
+                return getMyPlatforms();
+            },
 
+            addToMyPlatforms: function (platform, callback) {
+                return addToMyPlatforms(platform, callback);
+            },
+
+            removeFromMyPlatforms: function (platform, callback) {
+                return removeFromMyPlatforms(platform, callback);
             }
         }
 
     });
-
